8234c2dde3e5a4df14c9f5d0e3176350b3aed36a
Junichi Okamura <junichi.okamura@shopify.com>
2020-03-04 22:47:09 +0900

    #

--- public/tax.js
+++ public/tax.js
@@ -1,18 +1,10 @@
-var txt = "";
-let nodes = document.evaluate("//a[contains(@href, '/products/%E5%95%86%E5%93%81%EF%BC%91')]//*[contains(., '¥1,000')]", 
-  document, null, XPathResult.ANY_TYPE, null );
-txt = nodes.iterateNext().textContent;
-  /*      while (result) {
-            txt += result.textContent;
-            result = nodes.iterateNext();
-}*/
-txt = txt.replace(/' '/g, '').replace(/"/g, '').replace(/'/g, '');
-alert(txt);
 
-let nodes2 = document.evaluate("//a[contains(@href, '/products/%E5%95%86%E5%93%81%EF%BC%91')]//*[contains(., '¥1,000')]", 
-  document, null, XPathResult.ANY_TYPE, null );
-var result2 = nodes2.iterateNext();
-result2.textContent = "222222";
+let root_query = "//a[contains(@href, '/products/%E5%95%86%E5%93%81%EF%BC%91')]//*[contains(., '¥1,000')]";
 
+var org_price = document.evaluate(`${root_query}/text()`, document, null, XPathResult.ANY_TYPE, null ).iterateNext().nodeValue().
+  replace(/' '/g, '').replace(/"/g, '').replace(/'/g, '').replace(/¥/g, '');
+alert(org_price);
+
+document.evaluate(root_query, document, null, XPathResult.ANY_TYPE, null ).iterateNext().textContent = parseInt(org_price) + 333;
 
 
