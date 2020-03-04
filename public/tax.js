71c68cc9d8d0f011c6b8a45de2819f8bef359d74
Junichi Okamura <junichi.okamura@shopify.com>
2020-03-04 22:55:08 +0900

    #

--- public/tax.js
+++ public/tax.js
@@ -1,10 +1,16 @@
 
 let root_query = "//a[contains(@href, '/products/%E5%95%86%E5%93%81%EF%BC%91')]//*[contains(., '¥1,000')]";
 
-var org_price = document.evaluate(`${root_query}/text()`, document, null, XPathResult.ANY_TYPE, null ).iterateNext().nodeValue.
-  replace(/' '/g, '').replace(/"/g, '').replace(/'/g, '').replace(/¥/g, '');
-alert(org_price);
+var price_text = "";
+var text_nodes = document.evaluate(`${root_query}/text()`, document, null, XPathResult.ANY_TYPE, null);
+var n = text_nodes.iterateNext();
+while (n) {
+    price_text += n.nodeValue;
+    n = text_nodes.iterateNext();
+}
+price_text = price_text.replace(/' '/g, '').replace(/"/g, '').replace(/'/g, '').replace(/¥/g, '');
+alert(price_text);
 
-document.evaluate(root_query, document, null, XPathResult.ANY_TYPE, null ).iterateNext().textContent = parseInt(org_price) + 333;
+document.evaluate(root_query, document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent = parseInt(price_text) + 333;
 
 
