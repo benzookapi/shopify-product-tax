a3dd9807ebf238555c39c6c38e050a0f5b6c66d0
Junichi Okamura <junichi.okamura@shopify.com>
2020-03-04 21:52:14 +0900

    #

--- public/tax.js
+++ public/tax.js
@@ -1,5 +1,5 @@
 var txt = "";
-let nodes = document.evaluate("//span[contains(@class, 'visually-hidden')]", document, null, XPathResult.ANY_TYPE, null );
+let nodes = document.evaluate("//span[@class='visually-hidden']", document, null, XPathResult.ANY_TYPE, null );
 var result = nodes.iterateNext();
         while (result) {
             txt += result.childNodes[0].nodeValue + "<br>";
