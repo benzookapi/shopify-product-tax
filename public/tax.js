09b16f6cef2a2f09f82e1a4e8dae26ba7f747c89
Junichi Okamura <junichi.okamura@shopify.com>
2020-03-04 23:57:34 +0900

    #

--- public/tax.js
+++ public/tax.js
@@ -50,8 +50,8 @@ for (let i = 0; i < 1; i++) {
       }
       text_value = textToValue(text);
       if (text_value != "") {
-          document.evaluate(root_query, document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent = 
-            `${text}  税込：${formatter.format(parseInt(text_value) * tax)}`;
+          //document.evaluate(root_query, document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent = 
+          //  `${text}  税込：${formatter.format(parseInt(text_value) * tax)}`;
       }
   }
 }
