5513e4cf019709297e9f101e10e3c16f7aae5919
Junichi Okamura <junichi.okamura@shopify.com>
2020-03-04 21:15:29 +0900

    #

--- public/tax.js
+++ public/tax.js
@@ -1,2 +1,2 @@
-let price = document.querySelector("a[href='/products/%E5%95%86%E5%93%81%EF%BC%91']");
+let price = document.querySelector("a[href='/products/%E5%95%86%E5%93%81%EF%BC%91'] p[class='grid-link__meta'] span[class='visually-hidden']");
 alert(price.innerHTML);
