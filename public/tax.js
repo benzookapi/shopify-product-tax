3e61e769a955535daf3ea3909ccc276b43f36d21
Junichi Okamura <junichi.okamura@shopify.com>
2020-03-04 23:18:17 +0900

    #

--- public/tax.js
+++ public/tax.js
@@ -1,16 +1,39 @@
 
-let root_query = "//a[contains(@href, '/products/%E5%95%86%E5%93%81%EF%BC%91')]//*[contains(., '¥1,000')]";
 
-var price_text = "";
+/* -- Key data for products -- */
+var product_path = '/products/%E5%95%86%E5%93%81%EF%BC%91';
+var prduct_price = '¥1,000';
+var tax = 1 + (parseInt('10') * 0.01);
+
+/* -- Top page -- */
+var root_query = `//a[contains(@href, ${product_path})]//*[contains(., ${prduct_price})]`;
+var text = "";
 var text_nodes = document.evaluate(`${root_query}/text()`, document, null, XPathResult.ANY_TYPE, null);
 var n = text_nodes.iterateNext();
 while (n) {
-    price_text += n.nodeValue;
+    text += n.nodeValue;
     n = text_nodes.iterateNext();
 }
-price_text = price_text.replace(/' '/g, '').replace(/"/g, '').replace(/'/g, '').replace(/¥/g, '').replace(/,/g, '');
-//alert(price_text);
+text = price_text.replace(/' '/g, '').replace(/"/g, '').replace(/'/g, '').replace(/¥/g, '').replace(/,/g, '');
+if (text != "") {
+    document.evaluate(root_query, document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent = parseInt(text) * tax;
+}
 
-document.evaluate(root_query, document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent = parseInt(price_text) + 333;
+/* -- Product page -- */
+if (window.location.pathname.endsWith(product_path)) {
+    root_query = "//*[@id='ProductPrice']";
+    text = "";
+    text_nodes = document.evaluate(`${root_query}/text()`, document, null, XPathResult.ANY_TYPE, null);
+    n = text_nodes.iterateNext();
+    while (n) {
+        text += n.nodeValue;
+        n = text_nodes.iterateNext();
+    }
+    text = price_text.replace(/' '/g, '').replace(/"/g, '').replace(/'/g, '').replace(/¥/g, '').replace(/,/g, '');
+    if (text != "") {
+        document.evaluate(root_query, document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent = parseInt(text) * tax;
+    }
+}
+
 
 
