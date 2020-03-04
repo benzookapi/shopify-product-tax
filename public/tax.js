bdebd97b680fb8261286b2976afa9945f1263b85
Junichi Okamura <junichi.okamura@shopify.com>
2020-03-04 21:46:12 +0900

    #

--- public/tax.js
+++ public/tax.js
@@ -1,7 +1,7 @@
 let product = document.querySelector("a[href$='/products/%E5%95%86%E5%93%81%EF%BC%91']");
 alert(product.innerHTML);
 var txt = "";
-let node = document.evaluate("//*[contains(text(), '¥1,000')]", document, null, XPathResult.ANY_TYPE, null );
+let nodes = document.evaluate("//*[contains(text(), '¥1,000')]", document, null, XPathResult.ANY_TYPE, null );
 var result = nodes.iterateNext();
         while (result) {
             txt += result.childNodes[0].nodeValue + "<br>";
