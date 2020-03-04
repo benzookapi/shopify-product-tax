654f31ecefbde552cef33dba12b0ef292fb3f055
Junichi Okamura <junichi.okamura@shopify.com>
2020-03-04 22:04:20 +0900

    #

--- public/tax.js
+++ public/tax.js
@@ -1,5 +1,5 @@
 var txt = "";
-let nodes = document.evaluate("//a[contains(@href, '/products/%E5%95%86%E5%93%81%EF%BC%91')]//*[@class='grid-link__meta']", document, null, XPathResult.ANY_TYPE, null );
+let nodes = document.evaluate("//a[contains(@href, '/products/%E5%95%86%E5%93%81%EF%BC%91')]//*[@class='grid-link__meta']/text()", document, null, XPathResult.ANY_TYPE, null );
 var result = nodes.iterateNext();
         while (result) {
             txt += result.childNodes[0].nodeValue + "<br>";
